8888d35094f78779584eee678fe130791c71e98e
kylecho <kylecho.work@gmail.com>
2015-09-12 12:33:47 -0700

    Passing tests and cleans up code

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -4,11 +4,10 @@ module.exports = {
   messages: {
     get: function (req, res) {
       models.messages.get( '*',function(err, response){
-        console.log('QUERY MESSAGES AND RECEIVED RESPONSE', response);
         if (!err) {
           res.end(JSON.stringify(response));
         } else {
-          console.log('THERE IS AN ERROR IN RETRIEVING MESSAGES. ', err);
+          console.log('SELECT ERROR: ', err);
         }
       });
     }, // a function which handles a get request for all messages
@@ -22,7 +21,6 @@ module.exports = {
 
       // insert message into database after successful ID retrieval
       var postMessage = function(id){
-        console.log('HERE IS BODY TO MESSAGE: ',req.body);
         var d = {id:id, message:req.body.message, roomname:req.body.roomname};
         models.messages.post(d, endResponse);
       };
@@ -30,7 +28,6 @@ module.exports = {
       // Get user id from users table
       var userId = models.users.get(req.body.username, function(record){
         var rec = record[0].id;
-        // console.log('RECORDS RETURNED AND ID IS: ', rec);
         postMessage(rec);
       });
 
@@ -43,10 +40,7 @@ module.exports = {
       models.users.get(req.body);
     },
     post: function (req, res) {
-      // var data = "";
-      // console.log('DONE CHUNKING AND HERE IS RESULT', req.body);
       models.users.post(req.body, function(response){
-        // console.log('SUCCESSFUL RESPONSE', response);
         res.end();
       });
     }
--- server/models/index.js
+++ server/models/index.js
@@ -9,38 +9,24 @@ module.exports = {
       query
         .on('error', function(err) {
           // Handle error, an 'end' event will be emitted after this as well
-          console.log('THERE IS AN ERROR IN STREAM QUERY', err);
           cb(err);
-        })
-        .on('fields', function(fields) {
-          // the field packets for the rows to follow
-
         })
         .on('result', function(row) {
           // Pausing the connnection is useful if your processing involves I/O
-          console.log('NEW ROWWWW', row);
           rows.push(row);
         })
         .on('end', function() {
           // all rows have been received
-          console.log('QUERY HAS ENDED SUCCESSFULLY', rows);
           cb(null, rows);
         });
 
-
-      // db.query("SELECT (?) FROM messages", [data], function(err, results){
-      //   console.log('Retrieved the message of :', results);
-      //   cb(err, results);
-      // });
     }, // a function which produces all the messages
     post: function (data, cb) {
-// | message | text  | user_id |  room_id |
-      db.query('INSERT INTO messages (message, user_id, roomname) VALUES ( ?, ?, ? )', [data.message, data.id, data.roomname], function(err, res){
-        console.log('ATTEMPT INSERT INTO MESSAGES TABLE: ', res, 'ERROR ?', err);
+      var query = 'INSERT INTO messages (message, user_id, roomname) VALUES ( ?, ?, ? )';
+
+      db.query(query, [data.message, data.id, data.roomname], function(err, res){
         cb(res);
       });
-      
-   
     } // a function which can be used to insert a message into the database
   },
 
@@ -49,9 +35,8 @@ module.exports = {
     get: function (data, cb) {
 
       db.query('SELECT * FROM `users` WHERE `name` = ?', [data], function(err, data){
-        console.log('Retrieved the username of :', data);
-        if(!err) {
-          console.log('QUERY ERROR: ',err );
+        if (!err) {
+          console.log('QUERY ERROR: ', err);
         }
         cb(data);
       });
